5cb519e0de244a1dcbd0fe3a8aaa7622f6b28611
Andrey Sergeev <50415943+sergeevsite@users.noreply.github.com>
2020-06-07 23:27:27 +0700

    update pravosite 07/06

--- js/init.js
+++ js/init.js
@@ -22,18 +22,39 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   });
   
-  if(window.innerWidth < 990) {
-    $('.header-bottom-menu li').each((i, item) => {
-      if($(item).find('ul').length > 0) {
-        console.log(item);
-        $(item).append('<div class="header-bottom-menu__arrow"></div>');
-        $(item).find('div').click(() => {
-          $(item).closest('li').find('ul').eq(0).slideToggle();
-          $(item).toggleClass('open');
-        });
+  //Адаптивное выпадающее меню
+  let mainMenu = document.querySelector('.main-menu');
+  let arrowMenu = document.querySelectorAll('.arrow');
+
+  if(window.matchMedia("(max-width: 990px)").matches){
+    mainMenu.classList.add('touch');
+    for(i = 0; i < arrowMenu.length; i++){
+      let subMenu = arrowMenu[i].nextElementSibling;
+      arrowMenu[i].addEventListener('click', (event) => {
+        subMenu.classList.toggle('open');
+        event.target.classList.toggle('active');
+        event.target.previousElementSibling.classList.toggle('active');
+      });
+    }
+  }else{
+    mainMenu.classList.add('desktop');
+    if(arrowMenu.length !== 0) {
+      for(i = 0; i < arrowMenu.length; i++){
+        arrowMenu[i].previousElementSibling.classList.add('active');
       }
+    }
+  }
+
+  //Faq Section
+  if($("#faqSection").length !== 0) {
+    $("#faqSection").accordion({
+      collapsible: true,
+      heightStyle: "content",
+      active: false
     });
   }
+
+
   // $('.tooltip').tooltip({
   //   show: { effect: "hightlight", duration: 200 },
   //   hide: { effect: "drop", duration: 300 },
